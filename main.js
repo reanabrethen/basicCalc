61105829abb6167246124a631748740d4373ec3f
Admin <reanajb@gmail.com>
2024-01-31 12:32:59 -0500

    some functionality

--- main.js
+++ main.js
@@ -18,11 +18,37 @@ const decimal = document.querySelector('.decimal')
 
 const operators = document.querySelectorAll('.operator')
 
-
+const equal = document.querySelector('#equal-button')
 
 //EVENT LISTENERS
 
 clear.addEventListener('click', clearDisplay )
+
+for(let num of numbers){
+    num.addEventListener('click', numberPressed)}
+
+
+for(let op of operators){
+    op.addEventListener('click', operationPressed)}
+    
+
+    equal.addEventListener('click', calcResult)
+
+// display.addEventListener('click', updateScreen)
+
+
+
+
+
+
+// numbers.addEventListener('click', numberPressed)
+//  for(let element of numberPressed){
+//         element.addEventListener('click', (event)=>{
+//             firstNumber = event.target.innerText
+//             secondNumber = event.target.innerText
+//         })
+//     }
+
 
 // numbers.addEventListener('click', numberPressed)  need to loop through it
 
@@ -31,34 +57,52 @@ clear.addEventListener('click', clearDisplay )
 //FUNCTIONS
 
 function calcResult(){
-
+//switch operation? 
+//eval function?
+// let operators = ''
+//    if(operators === + ){
+//     eval(a + b)
+// }else if(operators = '-'){
+//     eval(a-b)
+// }else if(operators = '/'){
+//     eval(a/b)
+// }else if(operators = '*'){
+//     eval(a*b)
+// } updateScreen()
 }
 
-function operationPressed(op){
 
-}
-
-function numberPressed(number){
-
-
-// if operator saved then second number
-let operator = ['+', '-', '/', '*']
-
-        for(let i = 0; i < number.length; i++){
-        if(firstNumber === '' && operator ){  //checking to see if operator exsists
-            firstNumber += number[i] 
-    }else if(secondNumber === ''){ //checking to see if operator exsists
-        secondNumber += number[i]
+function operationPressed(event){
+        operation = event.target.value
     }
-} return number
-}
+
+
+
+function numberPressed(event){
+// if operator saved then second number
+console.log(event.target.value)
+        if(operation === '' ){  //checking to see if operator exsists
+            firstNumber += event.target.value
+    }else{ //checking to see if operator exsists
+        secondNumber += event.target.value
+    } updateScreen()
+} 
 
 
 function clearDisplay(){
-    display.value = ''
+    display.innerText = ''
+    firstNumber = ''
+    secondNumber = ''
+    operation = ''
 }
 
 
 function updateScreen(){
-
+    // display.value = firstNumber + operation + secondNumber
+    // display.addEventListener('click', (event)=>{
+    //             firstNumber = event.target.value
+    //      console.log(updateScreen)
+    //    }) 
+       
+            display.innerText = firstNumber + operation + secondNumber
 }
